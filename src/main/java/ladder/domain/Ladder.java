659183b7e3725c4e9fb084396cacc30f928ead65
SoJeong <105548648+ozoqzm@users.noreply.github.com>
2024-07-14 14:27:08 +0900

    Ladder 작성

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,10 +1,20 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ladder {
-    private final List<Line> lines;
-    public Ladder(List<Line> lines) {
+    private List<Line> lines = new ArrayList<>();;
+
+    public Ladder(int height, int numOfVerticalLines) {
+        for (int i = 0; i < height; i++) {
+            lines.add(new Line(numOfVerticalLines));
+        }
+    }
+    public void setLines(List<Line> lines) {
         this.lines = lines;
+    }
+    public List<Line> getLines() {
+        return lines;
     }
 }
